990a30c305c6b984f9e3b3f21320e2aed4492e6b
astrocinco <santiagolps3@gmail.com>
2021-11-20 21:40:14 -0300

    .

--- heap.c
+++ heap.c
@@ -1,6 +1,6 @@
 // NOTAS INTRAEQUIPO -- BORRAR PARA ENTREGA
 // * No se si existe un estandar de resultados de notas de comparacion. Por ahora voy a usar que 
-//   cmp(elem1, elem2) da negativo cuando elem1 es menor, y pos cuando elem1 mayor (porque es así en strcmp)
+//   cmp(elem1, elem2) da negativo cuando elem1 es menor, y positivo cuando elem1 mayor (porque es así en strcmp)
 
 #include <stdbool.h> 
 #include <stddef.h> 
@@ -8,10 +8,12 @@
 
 #define CAP_INIC 50ul
 #define FACTOR_NVA_CAP 2ul
+#define FACTOR_CARGA_MINIMA 2
+
 
 // Funcion de algoritmo de ordenamiento heap_sort. No es parte del TDA-Heap
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
-
+    // HACER
 }
 
 
@@ -29,15 +31,15 @@ heap_t *heap_crear(cmp_func_t cmp){
     heap_t* nuevo_heap = malloc(sizeof(heap_t));
     if (nuevo_heap == NULL) return NULL;
 
-    nuevo_heap->cant = 0;
-    nuevo_heap->cmp = cmp;
-    nuevo_heap->tam = CAP_INIC;
-    
     nuevo_heap->datos = malloc(nuevo_heap->tam * sizeof(void*));
     if (nuevo_heap->datos == NULL) return NULL;
 
+    nuevo_heap->cant = 0;
+    nuevo_heap->cmp = cmp;
+    nuevo_heap->tam = CAP_INIC;
     return nuevo_heap;
 }
+
 
 // Función auxiliar que retorna true si el primer valor es mayor que el segundo.
 bool aux_cumple_heap(heap_t* heap, size_t posicion1, size_t posicion2){
@@ -69,11 +71,16 @@ void aux_upheap(){
 }
 
 
+void aux_downheap(){
+    // HACER
+}
+
+
 // Funcion auxiliar para ordenar los arreglos para que cumplan las condiciones del heap
-void NOMBRE_FUNC_AUX_ORDENAR(heap_t* heap){
-    // DEFINIR NOMBRE
+void aux_ordenar_arreglo_entero(heap_t* heap){
     // ESTA FUNCION ORDENA UN ARREGLO PARA QUE CUMPLA CON LAS CONDICIONES DE UN HEAP
 }
+
 
 // Función auxiliar de heap_guardar y heap_borrar
 void aux_redimensionar(heap_t* heap, size_t nueva_cap){
@@ -95,13 +102,16 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
     return nuevo_heap;
 }
 
+
 bool heap_esta_vacio(const heap_t *heap){
     return heap->cant == 0;
 }
 
+
 size_t heap_cantidad(const heap_t *heap){
     return heap->cant;
 }
+
 
 bool heap_encolar(heap_t *heap, void *elem){
     heap->datos[heap->cant] = elem;
@@ -111,30 +121,32 @@ bool heap_encolar(heap_t *heap, void *elem){
     if (!cumple_heap(heap, pos_padre, pos_nuevo)){
         aux_upheap();
     }
-    // Insertar elemento en arreglo
-    // Reordenar para que cumpla con condiciones de heap -- HACER FUNCION AUX --
     heap->cant++;
     if (heap->cant == heap->tam) aux_redimensionar(heap, heap->tam * FACTOR_NVA_CAP);
-    // Revisar que no haya que redimensionar -- HACER FUNCION AUX --
-    return true; // ¿Return false si no se pudo redimensionar? Habría que redimensionar antes de guardar?
+    return true; 
 }
 
+
 void *heap_desencolar(heap_t *heap){
-    // ¿Pertenece?
+    if (heap->cant == 0) return NULL;
     void* dato_a_retornar; // = GUARDAR DATO
     // ? Sacar de arreglo
     // Reordenar para que cumpla con condiciones de heap -- HACER FUNCION AUX --
     heap->cant--;
-    // Revisar si hay que redimensionar -- HACER FUNCION AUX --
+    if (heap->cant == heap->tam / FACTOR_CARGA_MINIMA) aux_redimensionar(heap, heap->tam / FACTOR_NVA_CAP);
     return dato_a_retornar;
 }
 
+
 void *heap_ver_max(const heap_t *heap){
-    // Hay que retornar heap->datos[0] ¿No?
+    return heap->datos[0];
 }
 
+
 void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)){
-    // Iterar todo el arreglo y destruir los elementos con destruir_elemento
+    for (size_t i = 0; i < heap->datos; i++){ // Revisar que no sea heap->datos +1
+        destruir_elemento(heap->datos[i]);
+    }
     free(heap->datos);
     free(heap);
 }
